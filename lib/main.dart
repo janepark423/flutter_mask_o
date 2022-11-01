95f0752afcab32a16fa7a85e59c65318fd3994b0
jain <janepark@webiznet.com>
2022-11-01 15:22:56 +0900

    마스크 수량 색깔별 분류

--- lib/main.dart
+++ lib/main.dart
@@ -89,10 +89,48 @@ class _MyHomePageState extends State<MyHomePage> {
   }
 
   Widget _buildRemainStatWidget(Store store) {
+    var remainStat = '판매중지';
+    var description = ' ';
+    var color = Colors.black;
+    if (store.remainStat == 'plenty') {
+      remainStat = '충분';
+      description = '100개 이상';
+      color = Colors.green;
+    }
+    switch (store.remainStat) {
+      case 'plenty':
+        remainStat = '충분';
+        description = '100개 이상';
+        color = Colors.green;
+        break;
+      case 'some':
+        remainStat = '보통';
+        description = '30 ~ 100개';
+        color = Colors.yellow;
+        break;
+      case 'few':
+        remainStat = '부족';
+        description = '2~30개 이상';
+        color = Colors.red;
+        break;
+      case 'empty':
+        remainStat = '소진임박';
+        description = '1개 이하';
+        color = Colors.grey;
+        break;
+      default:
+    }
+
     return Column(
       children: <Widget>[
-        Text(store.remainStat ?? '매진'),
-        Text('100개'),
+        Text(
+          remainStat,
+          style: TextStyle(color: color, fontWeight: FontWeight.bold),
+        ),
+        Text(
+          description,
+          style: TextStyle(color: color),
+        ),
       ],
     );
   }
