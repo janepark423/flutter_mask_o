ee399f352667b91ba54c8ce97925b5877e8c8b58
jain <janepark@webiznet.com>
2022-11-01 17:35:40 +0900

    로딩바 구현

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,5 @@
-import 'dart:convert';
 import 'package:flutter/material.dart';
-import 'package:flutter_mask/viewmodel/store_model.dart';
-import 'package:http/http.dart' as http;
-import 'package:flutter_mask/model/store.dart';
+import 'package:flutter_mask/ui/view/main_page.dart';
 import 'package:flutter_mask/viewmodel/store_model.dart';
 import 'package:provider/provider.dart';
 
@@ -19,105 +16,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(),
-    );
-  }
-}
-
-class MyHomePage extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    final storeModel = Provider.of<StoreModel>(context);
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('마스크 재고 있는 곳 : ${storeModel.stores.where((e) {
-          return e.remainStat == 'plenty' ||
-              e.remainStat == 'some' ||
-              e.remainStat == 'few';
-        }).length}곳'),
-        actions: <Widget>[
-          IconButton(
-            icon: Icon(Icons.refresh),
-            onPressed: () {
-              storeModel.fetch();
-            },
-          )
-        ],
-      ),
-      body: storeModel.isLoading == true
-          ? loadingWidget()
-          : ListView(
-              children: storeModel.stores.where((e) {
-                return e.remainStat == 'plenty' ||
-                    e.remainStat == 'some' ||
-                    e.remainStat == 'few';
-              }).map((e) {
-                return ListTile(
-                  title: Text(e.name ?? ''),
-                  subtitle: Text(e.addr ?? ''),
-                  trailing: _buildRemainStatWidget(e),
-                );
-              }).toList(),
-            ),
-    );
-  }
-
-  Widget _buildRemainStatWidget(Store store) {
-    var remainStat = '판매중지';
-    var description = ' ';
-    var color = Colors.black;
-    if (store.remainStat == 'plenty') {
-      remainStat = '충분';
-      description = '100개 이상';
-      color = Colors.green;
-    }
-    switch (store.remainStat) {
-      case 'plenty':
-        remainStat = '충분';
-        description = '100개 이상';
-        color = Colors.green;
-        break;
-      case 'some':
-        remainStat = '보통';
-        description = '30 ~ 100개';
-        color = Colors.yellow;
-        break;
-      case 'few':
-        remainStat = '부족';
-        description = '2~30개 이상';
-        color = Colors.red;
-        break;
-      case 'empty':
-        remainStat = '소진임박';
-        description = '1개 이하';
-        color = Colors.grey;
-        break;
-      default:
-    }
-
-    return Column(
-      children: <Widget>[
-        Text(
-          remainStat,
-          style: TextStyle(color: color, fontWeight: FontWeight.bold),
-        ),
-        Text(
-          description,
-          style: TextStyle(color: color),
-        ),
-      ],
-    );
-  }
-
-  Widget loadingWidget() {
-    return Center(
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: <Widget>[
-          Text('정보를 가져오는 중'),
-          CircularProgressIndicator(),
-        ],
-      ),
+      home: MainPage(),
     );
   }
 }
