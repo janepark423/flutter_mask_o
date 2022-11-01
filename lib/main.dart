d30d43d33aade8d5cd3d61fbf866e106ce4aba59
jain <janepark@webiznet.com>
2022-11-01 15:10:31 +0900

    칠드런 위젯 text 생성

--- lib/main.dart
+++ lib/main.dart
@@ -29,10 +29,14 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
 //final stores = List<Store>();
   final stores = <Store>[];
+  var isLoading = true;
 
   Future fetch() async {
-//async와 await는 future 함수 안에서만 사용가능하다.
+    setState(() {
+      isLoading = true;
+    });
 
+//async와 await는 future 함수 안에서만 사용가능하다.
     var url =
         'https://gist.githubusercontent.com/junsuk5/bb7485d5f70974deee920b8f0cd1e2f0/raw/063f64d9b343120c2cb01a6555cf9b38761b1d94/sample.json';
     // 'https://gist.githubusercontent.com/junsuk5/bb7485d5f70974deee920b8f0cd1e2f0/raw/063f64d9b343120c2cb01a6555cf9b38761b1d94/sample.json?lat=37.266389&lng=126.999333&m=5000';
@@ -47,7 +51,9 @@ class _MyHomePageState extends State<MyHomePage> {
       jsonStores.forEach((e) {
         stores.add(Store.fromJson(e));
       });
+      isLoading = false;
     });
+    print('fetch 완료');
   }
 
   @override
@@ -60,16 +66,45 @@ class _MyHomePageState extends State<MyHomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('마스크 재고 있는 곳 : 0곳'),
+        title: Text('마스크 재고 있는 곳 : ${stores.length}곳'),
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(Icons.refresh),
+            onPressed: fetch,
+          )
+        ],
       ),
-      body: ListView(
-        children: stores.map((e) {
-          return ListTile(
-            title: Text(e.name ?? ''),
-            subtitle: Text(e.addr ?? ''),
-            trailing: Text(e.remainStat ?? '매진'),
-          );
-        }).toList(),
+      body: isLoading
+          ? loadingWidget()
+          : ListView(
+              children: stores.map((e) {
+                return ListTile(
+                  title: Text(e.name ?? ''),
+                  subtitle: Text(e.addr ?? ''),
+                  trailing: _buildRemainStatWidget(e),
+                );
+              }).toList(),
+            ),
+    );
+  }
+
+  Widget _buildRemainStatWidget(Store store) {
+    return Column(
+      children: <Widget>[
+        Text(store.remainStat ?? '매진'),
+        Text('100개'),
+      ],
+    );
+  }
+
+  Widget loadingWidget() {
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          Text('정보를 가져오는 중'),
+          CircularProgressIndicator(),
+        ],
       ),
     );
   }
